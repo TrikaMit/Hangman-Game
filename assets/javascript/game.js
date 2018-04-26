832cc072e2c149bd66e7358b96a232d93deab7ef
Treeks <trikamitra@gmail.com>
2018-04-26 02:26:11 -0400

    JavaScript win/lose functions fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,21 @@
-var muscles = ['deltoid','pectoralismajor','rectusabdominis','adductor','abductor','trapezius','vastusmedialis','tibialis','teresminor','infraspinatus','tricepsbrachii','gluteusmaximus'];
-console.log(muscles);
+startGame()
+function startGame (){
+
+var muscles = [
+
+    'deltoid', 
+    'pectoralismajor', 
+    'rectusabdominis', 
+    'adductor', 
+    'abductor', 
+    'trapezius', 
+    'vastusmedialis', 
+    'tibialis', 
+    'teresminor', 
+    'infraspinatus', 
+    'tricepsbrachii', 
+    'gluteusmaximus'
+];
 
 //listing all variables
 var star = "*"
@@ -7,112 +23,126 @@ var starArray = [];
 var whatLetter;
 var lowerLetter;
 var switcher;
-var score = 0;
+var score;
+var lives;
+var remainingLetters;
+var picker;
+var pickMuscle;
 var lives = 5;
+if (wins=0){
+    var score = 0;
+}else {
+    var score = 0+wins;
+}
+
+var wins;
+
+
+var picker = Math.floor(Math.random() * muscles.length)
+
+var pickMuscle = muscles[picker];
+console.log(pickMuscle);
 
 document.getElementById('score').innerHTML = "SCORE = " + score;
-document.getElementById('lives').innerHTML = "LIVES = " + lives;
+document.getElementById('lives').innerHTML = "LIVES = " + lives; 
 
 
-//This picks a random point from the string
-var picker = Math.floor(Math.random() * muscles.length)
-console.log(picker)
-//this picks the muscle corresponding to point that player is guessing
-var pickMuscle = muscles[picker] 
-console.log(pickMuscle) 
 
 //clicking guess box to display muscle
- document.getElementById('new-muscle').onclick = function() { 
-     newMuscle()}; 
- function newMuscle() {
-    // var starMuscle = star.repeat(pickMuscle.length)
-//     console.log(starMuscle);
-     document.getElementById('answer-box').innerHTML = starArray.join(" ");
- }
 
-//displays appropriate number of asterisks when guess box clicked
+/*     document.getElementById('new-muscle').onclick = function () {
+        newMuscle()
+    };
+    function newMuscle() {
+        // var starMuscle = star.repeat(pickMuscle.length)
+        //     console.log(starMuscle);
+        //displays appropriate number of asterisks when guess box clicked
+        document.getElementById('answer-box').innerHTML = starArray.join(" ");
+    } */
 
 for (var i = 0; i < pickMuscle.length; i++) {
     starArray[i] = "*";
     console.log(starArray);
-   }
-var starNumber = starArray[pickMuscle.length - 1];
-//console.log(starNumber);
+    document.getElementById('answer-box').innerHTML = starArray.join(" ");
+
+}
 var remainingLetters = pickMuscle.length;
-
-
 
 
 //the game play is kept within the pick Letter function
 //alerts for guess when clicked
- document.getElementById('guess-box').onclick = function() { 
-     pickLetter()};
- 
+
+document.getElementById('guess-box').onclick = function () {
+    pickLetter()
+};
+
 function pickLetter() {
-     //Asks user for a letter input and converts to lower case
-     var whatLetter = prompt('Please select a letter');
-     //console.log(whatLetter);
-     if (whatLetter!==null) {
-        var lowerLetter = whatLetter.toLowerCase();
-        console.log(lowerLetter);  
-     }
-     //game options
-     if (whatLetter===null){
-        alert("try again");
-    } else if (lowerLetter.length !== 1) {
-        alert ("Please enter a single letter guess");
-    } else {
+    //Asks user for a letter input and converts to lower case
+    var whatLetter = prompt('Please select a letter');
+    //console.log(whatLetter);
+    // if (whatLetter !== null) {
         
-        for (var j = 0; j < pickMuscle.length; j++) {
-    //correct guess
-            if (starArray.includes('*',0)) {
-                if (pickMuscle[j] === whatLetter ){
-            starArray[j] = whatLetter;
-            console.log(pickMuscle[j]);
-            remainingLetters--;
-            console.log(starArray);
-            document.getElementById('answer-box').innerHTML = starArray.join(" ");
-            //add score 
-                }    
-        }
-            else {
-                alert("You win!");
-                score += 1;
-                document.getElementById('score').innerHTML = "SCORE = " + score;
-                console.log(score);
+    // } 
+    if (whatLetter === null) {
+        alert("try again");
+    } else if (whatLetter.length !== 1) {
+        alert("Please enter a single letter guess");
+    } else { //if actual guess
+        var lowerLetter = whatLetter.toLowerCase();
+        if (lives > 0){
+            if (remainingLetters > 1){
+                for (var j = 0; j < pickMuscle.length; j++){
+                    if (pickMuscle[j] === lowerLetter) { //correct guess
+                        starArray[j] = lowerLetter;
+                        remainingLetters--;
+                        console.log(remainingLetters);
+                        document.getElementById('answer-box').innerHTML = starArray.join(" ");
+                    }else if (!pickMuscle.includes(whatLetter)){
+                        alert("WRONG!");
+                        lives -= 1;
+                        var wrongLetter = document.createElement('div');
+                        wrongLetter.innerHTML = lowerLetter;
+                        document.getElementById('incorrect-guesses').appendChild(wrongLetter);
+                        document.getElementById('lives').innerHTML = "LIVES = " + lives;
+                        break
+                    }
+                }
+            }else{
+                for (var j = 0; j < pickMuscle.length; j++){
+                    if (pickMuscle[j] === lowerLetter) { //correct guess
+                        starArray[j] = lowerLetter;
+                        document.getElementById('answer-box').innerHTML = starArray.join(" ");
+                        var youWin = alert('You win!');
+                        score += 1;
+                        wins += 1;
+                        document.getElementById('score').innerHTML = "SCORE = " + score;
+                        console.log(score);
+                        
+                        var playAgain = confirm("Do you want to play again?");
+                        if (playAgain){
+                           score += 1;
+                           document.getElementById('score').innerHTML = "SCORE = " + score;
+                           startGame(score += 1); 
+
+                          
+                        }
+
+                    }
+                }
             }
-        }    
+        }else{
+            alert("The correct answer was " + pickMuscle);
+            var youLose = alert("You lose!");
+        
+            var rePlay = confirm("Play again?");
+            if (rePlay){
+                location.reload()
+            }
+        }
     }
-    //input if WRONG
-   
- }
+}
 
 
+}
 
 
-//if they correctly pick a letter
-// function youRight() {
-//     var switcher = starMuscle.replace(starMuscle, whatLetter);
-//     console.log(switcher);
-//     document.getElementById('answer-box').innerHTML = switcher;
-// }
-
-// if (pickMuscle.includes(lowerletter)) {
-//     console.log('this is working')
-//     youRight();
-// }
-
-
-
-
-
-
-
-//This will switch whatever with whatLetter
-// var switcher = pickMuscle.replace(pickMuscle[1], whatLetter);
-// console.log(switcher);
-
-
-//Will need this to replace asterisk with correct letter
-//str = "Please visit Microsoft and Microsoft!";
-//var n = str.replace(/Microsoft/g, "W3Schools");
